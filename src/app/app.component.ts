907688e75bb3dad518259c15802c5859641b470a
shashankmn <shashank.1si12cs111@gmail.com>
2017-05-29 12:49:46 +0530

    added a heroes array, used *ngFor to display the list of heroes in the array and added the css to the list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,16 +5,35 @@ export class Hero {
   name: string;
 }
 
+//creation of the constant hero array of type Hero
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'Tour of Heroes';
   hero: Hero = {
     id: 1,
     name: "Windstorm"
   };
+//exposing heroes as a public property of the class
+//the type for heroes is infered from the HEROES array itself
+  heroes = HEROES;
 
 }
